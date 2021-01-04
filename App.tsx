5272ab1c21485ef44920c9310aec3d47551a8561
yeli7289 <yeli7289@colorado.edu>
2021-01-03 19:55:57 -0800

    config

--- App.tsx
+++ App.tsx
@@ -8,12 +8,13 @@ import Navigation from './navigation';
 import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthticationNavigationView from './navigation/index';
+import * as config from './config.json';
 
 export default function App() {
   const colorScheme = useColorScheme();
   const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache: new InMemoryCache(), 
-    uri: 'https://6hp1exkdk2.execute-api.us-east-1.amazonaws.com/v1/graphql'
+    uri: config.gql.uri
   });
   return (
     <ApolloProvider client={apolloClient}>
